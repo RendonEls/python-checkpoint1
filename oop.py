6577eba3a21f7708b4f0355f6da02fd0479628a2
Rendon Elsesser <rendonelsesser@gmail.com>
2019-03-04 10:13:34 -0500

    commit

--- oop.py
+++ oop.py
@@ -18,6 +18,29 @@
 # Your Vehicle class should take one argument (a `dict`) with the above
 # attributes. Define the properties on the class from the dict that is passed in.
 
+class Vehicle: 
+    def __init__(self, vehicle_type, wheel_count, name, cost, colors, vehicle_brand):
+        self.vehicle_type = vehicle_type
+        self.wheel_count = wheel_count
+        self.name = name
+        self.cost = cost
+        self.colors = colors
+        self.vehicle_brand = vehicle_brand
+        self.mpg = {
+            "city": '',
+            "highway": '',
+            "combined": ''
+        }
+
+    def get_vehicle_type(self):
+        print(self.vehicle_type)
+    def get_vehicle_brand(self):
+        print(self.vehicle_brand)
+    def get_vehicle_drive(self):
+        if self.wheel_count == "no wheels!":
+            print("no wheels send it back to the shop")
+        else: 
+            print(f"i have {self.wheel_count} wheel drive")
 
 
 # #2: Create a Motorcycle class that inherits from the Vehicle class and has the
@@ -26,18 +49,25 @@
 # - method: `pop_wheelie` if `wheel_count` is not equal to 2 then it should be False,
 #       otherwise return "......pop!"
 
+class Motorcycle(Vehicle):
+    pass
 
 
 # #3: Define a Car class that inherits from the vehicle class with the following attributes and methods:
 # - property: `wheel_count` defaults to 4
 # - method: `can_drive` that should return 'Vrrooooom Vroooom'
 
+class Car(Vehicle):
+    def __init__(self):
+        pass
 
 
 # #4: Define a Truck class that inherits from the vehicle class with the following attributes and methods:
 # - property: `wheel_count` defaults to "no wheels!"
 # - method: `rev_engine` that should return 'revvvvvreeeev'
 
-
+class Truck(Vehicle):
+    def __init__(self, wheel_count):
+        pass
 
 # Commit when you finish working on these questions!
